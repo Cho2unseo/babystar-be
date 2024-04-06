f6bf9d2e32a46c1e8253864b727805965458c166
Cho2unseo <escces00@gmail.com>
2024-04-06 21:12:29 +0900

    first commit

---
+++ src/main/java/baby/ey/Application.java
@@ -0,0 +1,13 @@
+package baby.ey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/baby/ey/ApplicationTests.java
@@ -0,0 +1,13 @@
+package baby.ey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
