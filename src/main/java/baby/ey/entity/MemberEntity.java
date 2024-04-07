04bef6e8c242a2d114b649eeb944f3238e4e0548
Cho2unseo <escces00@gmail.com>
2024-04-08 00:08:42 +0900

    feat: 엔티티와 회원가입

---
+++ src/main/java/baby/ey/entity/MemberEntity.java
@@ -0,0 +1,35 @@
+package baby.ey.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class MemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String userid;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 50, nullable = false)
+    private String email;
+
+    // FK 추가해야함
+
+    @Builder
+    public MemberEntity(String userid, String password, String email) {
+        this.userid = userid;
+        this.password = password;
+        this.email = email;
+    }
+
+}
