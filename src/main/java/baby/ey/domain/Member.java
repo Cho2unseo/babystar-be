8c9cedfe121e9d23d8b4d20f82c90a88e8d3b7c9
Cho2unseo <escces00@gmail.com>
2024-04-09 20:23:24 +0900

    hahaha

--- src/main/java/baby/ey/domain/Member.java
+++ src/main/java/baby/ey/domain/Member.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode(of = "id")
