7827c50d9db41e246004e37b83909f4f1a64cdcd
Cho2unseo <escces00@gmail.com>
2024-05-11 09:40:52 +0900

    tmp

--- src/main/java/baby/ey/crying/controller/CryingController.java
+++ src/main/java/baby/ey/crying/controller/CryingController.java
@@ -6,11 +6,13 @@ import baby.ey.crying.service.CryingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -27,7 +29,8 @@ public class CryingController {
 
     @PostMapping("/api/crying/post")
     @Operation(summary = "아기 울음소리 작성", description = "아기 울음소리 기록 API")
-    public CryingResponseDto createCrying(@RequestBody CryingRequestsDto requestDto) {
-        return cryingService.createCrying(requestDto);
+    public CryingResponseDto createCrying(@RequestBody CryingRequestsDto requestDto) throws IOException {
+        String imageUrl = cryingService.uploadCrying(requestDto.getPath());
+        return cryingService.createCrying(imageUrl);
     }
 }
--- src/main/java/baby/ey/crying/dto/CryingRequestsDto.java
+++ src/main/java/baby/ey/crying/dto/CryingRequestsDto.java
@@ -1,13 +1,15 @@
 package baby.ey.crying.dto;
 
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class CryingRequestsDto {
     private Long id;
-    private String path;
+    // private String path;
+    private MultipartFile path;
     private String content;
     private LocalDateTime created;
 }
